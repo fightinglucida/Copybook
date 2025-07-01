caa07ec593a05e42d61bf0a6d4eda8aa49d94cbf
Lucida <lucida1607@gmail.com>
2025-07-01 16:58:45 +0800

    修复加载css报错的问题

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,13 @@ module.exports = {
   content: [
     './layouts/**/*.html',
     './content/**/*.md',
+    './assets/**/*.html'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: true
+  }
 }
